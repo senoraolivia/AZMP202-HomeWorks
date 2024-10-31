9bd1e3da69d2f638c39261227aaa5ce12ddc43bb
senoraolivia <126396765+senoraolivia@users.noreply.github.com>
2024-10-31 15:32:54 +0400

    Loops tasks done

---
+++ 01.JsVariables.Control-sturucture.loops/tasks.js
@@ -0,0 +1,138 @@
+//TASK-1
+
+// let number = 789;
+// let sum = 0;
+// let hasil = 1;
+// let count = 0;
+
+// let deyisen = number;
+
+
+// while (deyisen > 0) {
+//     let regem =  deyisen % 10; 
+//     sum += regem;          
+//     hasil *= regem;      
+//     count++;              
+//     deyisen = (deyisen - regem) / 10; 
+// }
+
+
+// let average = sum / count;
+
+
+// console.log(" cəmi: " + sum);
+// console.log(" hasili: " + hasil);
+// console.log(" ədədi ortası: " + average);
+
+
+
+//TASK-2
+
+// let num=345
+
+// for (let i = 2; i< num/2; i++) {
+//     if (num%i===0) {
+//         console.log(i);
+        
+//     }
+
+// }
+
+
+
+// TASK-3
+
+// let num=800
+// count=0
+
+// for (let i = 2; i< num/2; i++) {
+//     if ( num%i===0) {
+//         count++;
+//     }
+// }
+
+// console.log(count);
+
+
+
+// TASK-4
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i + " x " + i + " = " + (i*i ));
+// }
+
+
+// TASK-5
+
+// yaza bilmedim
+
+
+// TASK-6
+
+// sumCut=0
+// sumTek=0
+
+// for (let i = 0; i <100 ; i++) {
+//     if (i%2===0) {
+//         sumCut+=i;
+//     }else {
+//         sumTek+=i;
+//     }
+
+// }
+
+// console.log("tekler: " + sumTek);
+// console.log("cutler: " + sumCut);
+
+
+
+// TASK-7
+
+// let arr= [4,2,7,9,3,12,46,32,19]
+// let arrMax= arr[0]
+// let arrMin=arr[0]
+// sum=0
+// let edediOrta;
+
+// for (let i = 0; i <arr.length ; i++) {
+//     if ( arrMax< arr[i]) {
+//         arrMax=arr[i];
+//     }else if (arrMin>arr[i]) {
+//         arrMin=arr[i];
+//     }
+
+// }
+
+// sum=arrMin+arrMax;
+// edediOrta=sum/2;
+
+//  console.log(edediOrta);
+
+
+// TASK-8
+
+// let sampleNews = `As software developers, the natural tendency is to start developing applications based on your ownhands-on experience and knowledge right away. However, overtime issues in the application arise, adaptations to changes,
+// and new features happen.`;
+// let count=0;
+
+// for (let i = 0; i <sampleNews.length ; i++) {
+//     if ( sampleNews[i]===' '){
+//         count++;
+
+//     }
+// }
+
+// console.log(count);
+
+
+
+
+
+
+ 
+
+
+
+
+
+
